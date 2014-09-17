56c169ed7e5abaf319148a56dd689747495dc388
shaalx <wisdomswing@gmail.com>
2014-09-17 17:22:23 +0800

    Revert "-"
    This reverts commit 2f0c974ee084ec9e65af287627c690ecd9ff38cc.

--- action/containAction/containAction.go
+++ action/containAction/containAction.go
@@ -11,5 +11,5 @@ func ContainAction(sequence string) {
 	path := "http://dict.youdao.com/mvoice?word=" + sequence
 	b := fetch.FetchData(&path, IP)
 	contain := search.SearchContain(b, "contain")
-	fmt.Println(string(b), contain)	
+	fmt.Println(string(b), contain)
 }
