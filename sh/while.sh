7e094746432b61c60940b15ede11e9c53d349774
shaalx <shaalx@yahoo.com.sg>
2015-10-31 15:21:53 +0000

    while

--- sh/while.sh
+++ sh/while.sh
@@ -5,8 +5,16 @@ do
 echo $i
 done
 
-let i=0
-while(($i < 10))
+for i in $(seq 1 10);do
+echo $i
+done
+
+for i in $[1..10];do
+echo $i
+done
+
+i=0
+while [ $i -lt 10 ]
 do
 i=$(($i+1))
 echo $i*$i
